b7ee3b7be6bf5c4a568b82e2edcfea28bbc3a18f
skyl1 <skyl1.maxla2@mail.ru>
2024-03-30 12:20:24 +0500

    update test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,9 +7,13 @@ def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
 
 
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -3, -1) == [1, 2]
     assert arrs.my_slice([], -3) == []
     assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
+
+
